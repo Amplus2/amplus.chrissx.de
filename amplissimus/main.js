ebc1ba02f0155d631f02ceb8cc4fc5f38104a1f0
Luddifee <info@ludwig-lehnert.de>
2020-06-18 12:21:48 +0200

    general update

--- amplissimus/main.js
+++ amplissimus/main.js
@@ -33,7 +33,7 @@ function closeNav() {
 
 function toggleDarkMode() {
 	isDarkMode = !isDarkMode;
-	document.querySelector("meta[name=theme-color]").setAttribute("content", isDarkMode ? "#111" : "#E8E8E8";
+	document.querySelector("meta[name=theme-color]").setAttribute("content", isDarkMode ? "#111" : "#E8E8E8");
 	getById("mainHeader").style.backgroundColor = isDarkMode ? "#111" : "#E8E8E8";
 	getById("navDrawerIcon").classList.toggle("dark-mode-foreground-svg");
 	document.body.classList.toggle("dark-mode-background");
