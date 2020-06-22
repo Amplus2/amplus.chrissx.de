06f292c2609e4bc2beba77706cd064f1abccce58
Luddifee <info@ludwig-lehnert.de>
2020-06-22 20:46:04 +0200

    general update

--- amplissimus/main.js
+++ amplissimus/main.js
@@ -135,5 +135,5 @@ function onLoadFunction() {
 		getById("mainBody").style.transitionDuration = "500ms";
 	}, 10);
 	updateHeight();
-	setTimeout(() => {openNav()}, 75);
+	setTimeout(() => {openNav()}, 500);
 }
